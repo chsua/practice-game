101b95069114edfd890a1e98d8e4e675d52d826a
chsua <csj1919@naver.com>
2022-12-15 19:12:05 +0900

    [feat] 상수 설정

---
+++ Constant.js
@@ -0,0 +1,15 @@
+const CAR = {
+    CARNAME_MIN: 1,
+    CARNAME_MAX: 5,
+}
+
+const RACE = {
+    GOSTANDARD: 4,
+}
+
+const MESSAGE = {
+    ERROR_CARNAME_LENGTH: "[ERROR]5자 이내로 입력해주세요.",
+    ERROR_RACECOUNT_LENGTH: "[ERROR]0회 이상으로 입력해주세요.",
+}
+
+module.exports = { CAR, RACE, MESSAGE } ;
