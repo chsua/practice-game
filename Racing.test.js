ad1a4285e730f8bf2ee744d96cf4862fe883fd31
chsua <csj1919@naver.com>
2022-12-15 19:47:33 +0900

    [test] car name validate test

--- Racing.test.js
+++ Racing.test.js
@@ -1,4 +1,5 @@
 const Car = require("./Car") ;
+const Racing = require("./Racing") ;
 
 describe("Car", () => {
     
@@ -6,5 +7,18 @@ describe("Car", () => {
         const car = new Car("hello") ;
         car.go(4);
         expect(car.spotPrint()).toBe("hello: -") ;
+    })
+})
+
+describe("Racing", () => {
+    
+    test("carNameValidate_예외처리 확인", () => {
+        const race = new Racing() ;
+        expect(() => {
+            race.carNameValidate("aaaaaa")
+        }).toThrow("[ERROR]") ;
+        expect(() => {
+            race.carNameValidate("")
+        }).toThrow("[ERROR]") ;
     })
 })
